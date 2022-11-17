ecdbef8ce7c1e6be1deb213fa55a958ecaa2f4d7
marcdudley17 <marc.dudley@utp.ac.pa>
2022-11-16 23:05:51 -0500

    SENSOR DE VOZ
    SENSOR DE VOZ

---
+++ app/src/main/java/com/example/proyfinal/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.proyfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bvoz, bcara, bhuella;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bvoz=findViewById(R.id.bvoz);
+
+        bvoz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivityVoz.class);
+                startActivity(intent);
+                }
+
+
+        });
+
+    }
+}
