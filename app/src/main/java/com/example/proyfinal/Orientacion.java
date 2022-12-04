7fbd1718119db8bd4b8e4a39dd50c38fdfdd0c7b
Dionisio246 <dionisiosantos24@gmail.com>
2022-12-04 14:53:59 -0500

    si sirve? XD

---
+++ app/src/main/java/com/example/proyfinal/Orientacion.java
@@ -0,0 +1,19 @@
+package com.example.proyfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orientacion extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
