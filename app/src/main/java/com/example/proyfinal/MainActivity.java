afbc4218c1942d45630f7e698d5c28b55a16a2aa
GodDgms <85698670+GodDgms@users.noreply.github.com>
2022-12-12 00:05:54 -0500

    aaaaaaaaaa

--- app/src/main/java/com/example/proyfinal/MainActivity.java
+++ app/src/main/java/com/example/proyfinal/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.proyfinal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button bvoz, bcara, bhuella;
+    private ImageButton bvoz, bcara, bhuella;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
