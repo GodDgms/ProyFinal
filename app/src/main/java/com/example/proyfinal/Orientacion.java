4529e679a38ec9c3840a605ceb6f6bd89e9d729d
Dionisio246 <dionisiosantos24@gmail.com>
2022-12-11 19:06:53 -0500

    Se resolvieron pequeños errores en el layout

--- app/src/main/java/com/example/proyfinal/Orientacion.java
+++ app/src/main/java/com/example/proyfinal/Orientacion.java
@@ -1,11 +1,23 @@
-package com.example.proyfinal;
+package com.example.orientacionfinal2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Orientacion extends AppCompatActivity {
+public class Orientacion extends AppCompatActivity implements SensorEventListener {
 
+    TextView tv1, tv2, tv3;
+
+
+    private SensorManager sensorManager;
+
+    private Sensor senAccelerometer;
 
 
     @Override
@@ -13,7 +25,30 @@ public class Orientacion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SensorManager senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv1.setText("X: " + x);
+        tv2.setText("Y: " + y);
+        tv3.setText("Z: " + z);
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
